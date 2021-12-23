8b8136bec24c39b6cbf97bc7f9f9c733f71487c1
zaki178 <74228332+zaki178@users.noreply.github.com>
2021-12-24 00:20:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -194,9 +194,9 @@ autoketik = false
 autoregister = setting.user_register
 
 img = setting.img
-baper = 'SUB YT ZEEONE OFC'
-apiku = 'https://youtu.be/2rWbjrDIAyA'
-gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
+baper = 'Z'
+apiku = 'wa.me/62895376867500'
+gc_wa_lu = 'https://wa.me/62895376867500' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
 owner = targetpc
 fake = setting.fake
